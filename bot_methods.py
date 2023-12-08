4f3ba0b072163a0f9e08a6715f357313ee7f82cd
Matheus Correa <matheus.barrosc@gmail.com>
2023-12-08 18:42:23 -0300

    Criação da funcionalidade de mensagens privadas.

--- bot_methods.py
+++ bot_methods.py
@@ -1,17 +1,14 @@
 import discord
 from discord.ext import commands
 from random import randint
-# Não finalizada
-class PrivateMessage(commands.Converter):
-    async def convert(self, ctx, argument):
-        if ctx.author.bot:
-            return
-        user = ctx.author
-        return 
 
-# Não finalizada
 async def command_help(arg):
-    pass
+    if arg == 'player':
+        player_creation = create_player()
+        return player_creation
+    elif arg == 'npc':
+        npc_creation = create_npc()
+        return npc_creation
 
 async def command_roll_dice(dice):
     wrong_command = """
@@ -26,10 +23,12 @@ Para rodar um dado, digite da seguinte forma o comando (variando o valor do dado
         dice_result = randint(1,int(dice_number))
         return dice_result
 
-# Não finalizada
-async def command_create_player(arg):
-    pass
+# Não Iniciada
+def create_player():
+    temporary_message = "Works"
+    return temporary_message
 
-# Não finalizada
-async def command_create_npc(arg):
-    pass
+# Não Iniciada
+def create_npc():
+    temporary_message = "Works"
+    return temporary_message
