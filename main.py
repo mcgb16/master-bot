af49a22104545c790c27934d0eca1ee67fdbe5f0
Matheus Correa <matheus.barrosc@gmail.com>
2023-12-11 17:40:33 -0300

    Ajustes no prefixo para comandos e no comando de help.

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import bot_methods
 
 intents = bot_methods.discord.Intents.default()
 intents.message_content = True
-bot = bot_methods.commands.Bot(command_prefix='#', intents=intents)
+bot = bot_methods.commands.Bot(command_prefix='?', intents=intents)
 
 @bot.command(name='roll')
 async def roll_dice(ctx, *, content: bot_methods.command_roll_dice = None):
-    if bot_methods.verify_bot_message(ctx) == None:
+    if bot_methods.help_methods.verify_bot_message(ctx) == None:
         return
-    elif bot_methods.verify_none(content) == None:
+    elif bot_methods.help_methods.verify_none(content) == None:
         return
     
     await ctx.send(await content)
@@ -17,13 +17,13 @@ async def roll_dice(ctx, *, content: bot_methods.command_roll_dice = None):
 # Não finalizado (devido as funções de criação)
 @bot.command(name='h')
 async def bot_help(ctx,*, content: bot_methods.command_help = None):
-    if bot_methods.verify_bot_message(ctx) == None:
-        return
-    elif bot_methods.verify_none(content) == None:
+    if bot_methods.help_methods.verify_bot_message(ctx) == None:
         return
 
-    error_message = f'Não foi possível enviar mensagem privada para {ctx.author}. Talvez as mensagens privadas estejam desativadas ou eu não tenho permissão para fazer isso.'
-    
+    if content == None:
+        content = bot_methods.command_help(None)
+
+    error_message = f'Não foi possível enviar mensagem privada para {ctx.author}. Talvez as mensagens privadas estejam desativadas ou eu não tenho permissão para fazer isso.'   
     success_message = 'Te mandei um zap, rs.'
 
     try:
