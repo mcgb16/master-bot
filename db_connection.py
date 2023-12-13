f3a6c2eb03ab24085f2ef61cb652e249f0befc78
Matheus Correa <matheus.barrosc@gmail.com>
2023-12-13 11:23:36 -0300

    Mudança de banco de dados utilizado de MySQL para SQLite3.

--- db_connection.py
+++ db_connection.py
@@ -1,58 +1,46 @@
-import mysql.connector
+import sqlite3
 import extras
-
-conn = mysql.connector.connect(
-    host=extras.mysql_host,
-    user=extras.mysql_user,
-    password=extras.mysql_pswd,
-    database=extras.mysql_db
-)
-cursor = conn.cursor()
 
 def create_tables():
     create_table_players = """
     CREATE TABLE IF NOT EXISTS players (
-        player_id int NOT NULL AUTO_INCREMENT,
-        player_name varchar(45) NOT NULL,
-        dexterity int NOT NULL,
-        strenght int NOT NULL,
-        constitution int NOT NULL,
-        intelligence int NOT NULL,
-        wisdom int NOT NULL,
-        charisma int NOT NULL,
-        hp int NOT NULL,
-        gold int DEFAULT 0,
-        PRIMARY KEY (player_id),
-        UNIQUE KEY player_id_UNIQUE (player_id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+        player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        player_name VARCHAR(45) NOT NULL,
+        dexterity INTEGER NOT NULL,
+        strenght INTEGER NOT NULL,
+        constitution INTEGER NOT NULL,
+        intelligence INTEGER NOT NULL,
+        wisdom INTEGER NOT NULL,
+        charisma INTEGER NOT NULL,
+        hp INTEGER NOT NULL,
+        gold INTEGER DEFAULT 0
+    )
     """
     create_table_npcs = """
     CREATE TABLE IF NOT EXISTS npcs (
-        npc_id int NOT NULL AUTO_INCREMENT,
-        npc_name varchar(45) NOT NULL,
-        dexterity int NOT NULL,
-        strenght int NOT NULL,
-        constitution int NOT NULL,
-        intelligence int NOT NULL,
-        wisdom int NOT NULL,
-        charisma int NOT NULL,
-        hp int NOT NULL,
-        gold int DEFAULT 0,
-        PRIMARY KEY (npc_id),
-        UNIQUE KEY npc_id_UNIQUE (npc_id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+        npc_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        npc_name VARCHAR(45) NOT NULL,
+        dexterity INTEGER NOT NULL,
+        strenght INTEGER NOT NULL,
+        constitution INTEGER NOT NULL,
+        intelligence INTEGER NOT NULL,
+        wisdom INTEGER NOT NULL,
+        charisma INTEGER NOT NULL,
+        hp INTEGER NOT NULL,
+        gold INTEGER DEFAULT 0
+    )
     """
+    return create_table_players, create_table_npcs
+
+with sqlite3.connect(extras.sqlite_db) as conn:
+    cursor = conn.cursor()
+
+    create_table_players, create_table_npcs = create_tables()
+
     try:
         cursor.execute(create_table_players)
         cursor.execute(create_table_npcs)
         conn.commit()
-        return
-    except Exception as e:
-        print(e)
+    except sqlite3.Error as e:
+        print("Erro SQLite:", e)
         conn.rollback()
-        return
-
-create_tables()
-
-cursor.close()
-conn.close()
